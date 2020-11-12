5ca0f80281719dc72579eb492af77f20ec62c24c
WadeeaKiwan <wadeea.kiwan@hotmail.com>
2020-11-12 23:59:06 +0100

    Update login page & create new build for deploy

---
+++ server/public/precache-manifest.739799b5ac38ab02f70f73420662b79b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993489e59ba2f1fdea3e3f8afd9f6dc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d22f23dbfcac135a2b75",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7b26dacf37af9775cc49",
+    "url": "/static/css/main.09f6aac3.chunk.css"
+  },
+  {
+    "revision": "d22f23dbfcac135a2b75",
+    "url": "/static/js/2.0f3ca311.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.0f3ca311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b26dacf37af9775cc49",
+    "url": "/static/js/main.8b992fa2.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
